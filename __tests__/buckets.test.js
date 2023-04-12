b8156a84aacbc8b55fc8cfadb0205e459153f5e3
Emma Gerigscott <elgerig@gmail.com>
2023-04-12 09:22:32 -0700

    pass test 5, intialize at level 1

--- __tests__/buckets.test.js
+++ __tests__/buckets.test.js
@@ -22,4 +22,8 @@ describe('Buckets', () => {
   test('should create an instance of Buckets with a sneakSnack() method that returns 0 when no arguments are passed', () => {
     expect(buckets.sneakSnack()).toEqual(0);
   });
+
+  test('should have property of level intialized at 1', () => {
+    expect(buckets.level).toEqual(1);
+  });
 })
--- src/buckets.js
+++ src/buckets.js
@@ -2,9 +2,10 @@ export default class Buckets {
   constructor() {
     this.type = "skinny pig";
     this.skills = ['sassy', 'snuggler', 'nibbler', 'dexterity'];
+    this.level = 1;
   }
 
   sneakSnack() {
-    return 0
+    return 0;
   }
 }
